def8df3d095239d44f71bec27897c29a80b5d65a
debopriyobhowmick <debopriyobhowmick@users.noreply.github.com>
2025-03-14 19:35:31 +0000

    deploy: b909a1a2ab6437135bca10f80b7695fbe34eb6f7

--- _next/static/PYHT0FsFxgOs77VrMv545/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-ad9d12cb8d2b0abf.js","static/chunks/620-17c262e7fd2dab6d.js","static/chunks/pages/index-298e9fadc987ca20.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
