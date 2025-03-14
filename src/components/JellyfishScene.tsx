b909a1a2ab6437135bca10f80b7695fbe34eb6f7
Debo macbook air <bhowmick.kushal@gmail.com>
2025-03-14 15:34:39 -0400

    decrease scale to 0.15

--- src/components/JellyfishScene.tsx
+++ src/components/JellyfishScene.tsx
@@ -50,7 +50,7 @@ function JellyfishModel() {
       <primitive 
         object={scene} 
         position={[0, 0, 0]}
-        scale={[0.25, 0.25, 0.25]} // Further reduced scale to 0.1
+        scale={[0.15, 0.15, 0.15]} // Further reduced scale to 0.1
       />
     </group>
   );
